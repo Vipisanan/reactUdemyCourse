d8750568fcb4cf52fbb1ba24cc26a5ddb5acae1f
vipisanan <vipisanan1@gmail.com>
2019-09-21 21:32:21 +0530

    first session done

--- src/component/App.js
+++ src/component/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form , FormControl , Button } from 'react-bootstrap';
-import  './App.css';
-
+import {Button, Form, FormControl} from 'react-bootstrap';
+import './App.css';
+import AgeState from "./AgeState";
 class App extends React.Component{
     constructor(){
         super();
         this.state={
-            newDate:''
+            newDate: '',
+            birthday: '1994-09-11'
         }
     }
     changedBirthday(){
         console.log(this.state);
+        this.setState({birthday: this.state.newDate})
     }
     render(){
         return(
-            <div className="App">
+            <div className="">
                 <Form inline>
                 <h2>Enter your Birthday</h2>
                 <FormControl type="date"
-                onChange={event => this.setState({newDate: event.target.value})}>
+                             onChange={event => this.setState({newDate: event.target.value})}>
                 </FormControl>
                 {''}
                 <Button onClick={() => this.changedBirthday()}>
                         Submit
                 </Button>
+                    <AgeState date={this.state.birthday}/>
            </Form>
             </div>
         );
