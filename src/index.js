4700efc4e95a8bed889dc3f0e7a746eedcc69514
vipisanan <vipisanan1@gmail.com>
2019-09-21 21:55:01 +0530

    go back to Zero bcs-> to do a redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './component/App'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom'
 
-
-ReactDOM.render(<App/> ,document.getElementById('root')
-)
+import App from "./components/App";
+ReactDOM.render(<App/> , document.getElementById('root'));
